b8b3065eb6a5a621fc674a454711d4231d076cd5
manasvvy <manasviisonone@gmail.com>
2026-07-02 22:43:08 +0530

    learned loops

---
+++ conditional_statements/conditional_exp.py
@@ -0,0 +1,8 @@
+marks=int(input("enter marks scored"))
+
+result="pass" if marks>35 else "fail"
+print(result)
+
+number=int(input("enter a number"))
+result= number**2 if number>=0 else number**3
+print(result)
---
+++ loops/range_ex.py
@@ -0,0 +1,24 @@
+r=range(10) #range(stop)
+print(r)
+print(type(r)) #<class 'range'>
+
+r2=range(20,26) #range(start,stop)
+print(r2)
+print(list(r2))
+
+r3=range(20,27,3) #range(start,stop,step)
+print(r3)
+print(list(r3))
+
+r4=range(-1,-11,-1)
+print(list(r4))
+
+r5=range(-10,6)
+print(list(r5))
+
+r6=range(-70,-50,3)
+print(list(r6))
+
+r7=range(8,-25,-5)
+print(list(r7))
+
