947900adc95fcb0a1aefa375587a861ad54284ae
Manasvi <manasviisonone@gmail.com>
2026-07-07 22:17:49 +0530

    Fix loop to break on first number divisible by 15

--- loops/break_control_statement.py
+++ loops/break_control_statement.py
@@ -1,7 +1,7 @@
 #print me numbers from 4 to 34, but stop the loop when the 1st number divisible by 15 is encountered
 
 print("start")
-for i in range(4,35):
+for i in range(4,35): 
     if i%15==0:
         break
     print(i)
