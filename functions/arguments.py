91e407fdd52a69c7d4ade19ccf6f7796b1199eca
manasvvy <manasviisonone@gmail.com>
2026-07-17 22:29:06 +0530

    Add function arguments and packing/unpacking examples

--- functions/arguments.py
+++ functions/arguments.py
@@ -70,15 +70,79 @@ print("----------------------------------------------------------")
                                 -> collects exxtra keyword arguments into a dict
                                 -> only one **kwargs should be used in function declaration
                                 -> **kwargs should be used after all types of parameters
-                                -> keywords arguments not matching the parameter name is also accepted in the dictionary'''
+                                -> keywords arguments not matching the parameter name is also accepted in the dictionary
+NOTE=IF WE USE **DICTIONARY IN THE FUNCTION CALL UNPACKING SHOULD BE SEEN
+THE PARAMETERS AND THEIR NAMES SHOULD MATCH THE KEYNAME IN THE DICT'''
 
-def collect(**kwargs):                  #**kwargs in function declaration
+def collect(**kwargs):                  #**kwargs in function declaration and it should be only placed on the right hand side 
     print(kwargs)
 collect(name="messi",age=39,goals=7)    # variable no of keywords arguments
 
+print("----------------------------------------------------------")
 
-'''4=default arguements
+def group(**kwargs):
+    print(kwargs)
+group(day="friday",time=8.45)
 
-'''                       
+print("----------------------------------------------------------")
+
+d={"name":"rm","age":29,"marks":94,"married":True}
+
+def disperse(name,age,marks,married):   #the parameter names should match the keynames
+    print(name,age,marks,married)
+disperse(**d)
+
+print("----------------------------------------------------------")
+
+d={"phy":90,"kannada":125,"maths":35,"python":98}
+
+def combine(**kwargs):                 #function declaration=**kwargs=packing dictionary
+    print(kwargs)
+combine(**d)                           #in function call only **dict=unpacking into the keyword arguements
+
+print("----------------------------------------------------------")
+
+def combine(python,**kwargs):           #function declaration=**kwargs=packing dictionary
+    print(kwargs)
+    print(python)
+combine(**d)                            #in function call only **dict=unpacking into the keyword arguements
+
+print("----------------------------------------------------------")
+
+
+'''4=default arguements -> it is a parameter in the function declaration that already has a predefined value #default
+                        -> if the function caller does not pass any value for that parameter then the default value will be used
+                        -> if the function caller passes a value for that parameter then the default value wont be used'''                       
+
+def evaluate(user="guest",marks=0,time=60):          #function declaration with default value
+    print(user,marks,time)                           #function body
+evaluate()                                           #function call
+evaluate("chotabheem")
+evaluate("sam",65)
+evaluate("chris",64,135)
+
+print("**********************************************************************")
+
+#combination of passing all types of arguements structure
+'''function declaration ---> function call
+1) positional parameters---> positional args
+2) *args                ---> any no of positional args
+3) default args         ---> keyword args
+4) **kwargs             ---> any no of keyword arguments'''
+
+def order(cname,hname,*starters,drinks="water",**mains):
+
+    print(hname)
+    print(starters)
+    print(drinks)
+    print(mains)
+    
+order("madhu","hotel cat","paneer","chicken","mutton chops",drinks="diet coke",rice="biryani",roti="tandoori", dessert="jamun",icecream="chocolate")    
+
+
+
+
+
+
 
 
