f11000a7d2c0d5d94535d507e99c239491cdaeb8
manasvvy <manasviisonone@gmail.com>
2026-07-24 11:20:19 +0530

    decorators modified and iterator started

--- functions/decorators.py
+++ functions/decorators.py
@@ -7,6 +7,24 @@ STEPS TO DEFINE A DECORATORS
 3)the outer func should return the address of the inner func
 outer function name=decorator, inner function name=wrapper
 '''
+#CONCEPTS USED IN THE DECORATORS
+#function
+#para and arguments
+#nested function
+#packing unpacking
+#function aliasing
+#higher order function
+#closure
+
+'''USES OF DECORATORS
+1. CODE REUSABILITY=> function can be reused multiple time w/o affecting the other logic
+2. SEPERATION OF CONCERNS=> decorators helps us seperate what a function does from how and when it is execute
+3. USEFUL FOR CROSSCUTTING CONCERNS=> certain behaviours apply across many parts of an applications such as:
+=logging
+=authentication
+=error handling
+=performance monitoring'''
+
 
 def decorator(fun):                 #decorator taking 1 para
     def wrapper():                  #wrapper containing decorators logic
@@ -19,6 +37,63 @@ def gift():                         #function to be decorated
 gift=decorator(gift)    
 gift()
 
+print("----------------------------------------------------------------")
 
+def decorator(cake):                                #decorator taking 1 para
+    def wrapper():                                  #wrapper containing decorators logic
+        print("choose the flavor of the cake")
+        cake()                                      #calling function is mandatory
+        print("pack the cake")
+    return wrapper                                  #returning wrapper is mandatory
+def bake_cake():                                    #function to be decorated
+    print("bake this cake")
+bake_cake=decorator(bake_cake)                      #decorator should have atleast 1 para() which should be another function
+bake_cake()                                         #wrapper internally and bake cake is extarnal
 
-    
+print("----------------------------------------------------------------")
+
+'''NOTE=> IF U WANNA PASS PARAMETER TO THE FUNCTION TO BE DECORATED
+1. FUNCTION TO BE DECORATED AND WRAPPER FUNCTION SHOULD HAVE EXACTLY SAME NO OF PARAMETER AND NAMES
+2. FIND THAT PARAMETER MUST BE USED IN THE ORIGINAL FUNCTION CALL
+3. WHEN CALLING THE DECORATOR FUNCTION PASS THE CORRECT NO OF ARGUMENTS AS PER PARAMETER
+'''
+
+def decorator(cake):                                #decorator taking 1 para
+    def wrapper(*args):                                  #wrapper containing decorators logic
+        print("choose the flavour of the cake")
+        cake(*args)                                      #calling function is mandatory
+        print("pack the cake")
+    return wrapper                                  #returning wrapper is mandatory
+def bake_cake(*args):                                    #function to be decorated
+    print("baked this cake", args)
+bake_cake=decorator(bake_cake)                      #decorator should have atleast 1 para() which should be another function
+bake_cake("egg","flour","baking soda","cocoa powder") #wrapper internally and bake cake is extarnal
+
+print("----------------------------------------------------------------")
+
+def decorator(cake):                                #decorator taking 1 para
+    def wrapper(*args,**kwargs):                                  #wrapper containing decorators logic
+        print("choose the flavour of the cake")
+        cake(*args,**kwargs)                                      #calling function is mandatory
+        print("pack the cake")
+    return wrapper                                  #returning wrapper is mandatory
+def bake_cake(*args,**kwargs):                                    #function to be decorated
+    print("baked this cake", args,kwargs)
+bake_cake=decorator(bake_cake)                      #decorator should have atleast 1 para() which should be another function
+bake_cake("egg","flour","baking soda","cocoa powder",milk="toned milk",essence="vanilla")                                         #wrapper internally and bake cake is extarnal
+
+print("----------------------------------------------------------------")
+
+def decorator(cake):                                
+    def wrapper(*args,**kwargs):                    #packing in fd                                
+        print("choose the flavour of the cake")
+        cake(*args,**kwargs)                        #unpacking in fc              
+        print("pack the cake")
+    return wrapper                                  #packing in fd
+
+@decorator                                          #@ automatically calls decorator 
+def bake_cake(*args,**kwargs):                                  
+    print("baked this cake", args,kwargs)
+#bake_cake=decorator(bake_cake)                     #manual decoration           
+bake_cake("egg","flour","baking soda","cocoa powder",milk="toned milk",essence="vanilla")#wrapper internally and bake cake is extarnal
+
---
+++ functions/iterator.py
@@ -0,0 +1,19 @@
+'''ITERATOR
+1-> object in python that allows to traverse/iterate to all the elements of a collection . one element at a time
+2-> iterator is unidirectional(allows to travers left to right)
+3-> it allows partial iteration
+
+THERE ARE 2 MAIN FUNCTIONS
+
+1. iter(collection)
+-> its a predefined function that takes collection as parameter and returns an iterator object
+->iterator object keeps a cursor that moves to the next element  every time next() is called
+
+2. next(iterator)
+-> predefined func that takes the iterable object and returns the next element from the iterator and advances (moves)
+the cursor
+->when no elements are left in the iterator it raises stopiteration
+
+NOTE= ITERABLE IS EXHAUSTIBLE (ONLY ONCE WE CAN ITERATE THRU IT)
+
+'''
