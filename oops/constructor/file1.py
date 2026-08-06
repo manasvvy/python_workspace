f284334d62ccb2df2d517eff8bc51925bc72ba94
manasvvy <manasviisonone@gmail.com>
2026-08-06 23:39:33 +0530

    modify

--- oops/constructor/file1.py
+++ oops/constructor/file1.py
@@ -8,3 +8,34 @@ p2=pen() #instance2
 
 print(p1.__dict__)
 print(p2.__dict__)
+
+print("----------------------------------------------------------")
+
+#w parameters
+
+class pen:
+    def __init__(self,name,cost):
+        self.name = name #iv1
+        self.cost = cost      #iv2
+
+p1=pen("doms",10) #instance1 pen()=constructor call  p1=reference
+p2=pen("apsara",16) #instance2
+
+print(p1.__dict__)
+print(p2.__dict__)
+
+#create friend instance w name loc and school
+
+class Friend:
+    def __init__(self,name,loc,school):
+        self.name = name #iv1
+        self.loc = loc      #iv2
+        self.school = school
+
+f1=Friend("chandu","blr","vgs") #instance1 pen()=constructor call  p1=reference
+f2=Friend("madhu","coorg","fhps") #instance2
+f3=Friend("kavya","pune","agps")
+
+print(f1.__dict__)
+print(f2.__dict__)
+print(f3.__dict__)
---
+++ oops/method/file1.py
@@ -0,0 +1,20 @@
+#class method
+
+class Teacher:
+    school="agps" #cv
+    principal="idkbro" #cv
+
+    @classmethod
+    def display_deets(cls):
+        print(cls.school) #printing is basically accessing the class variable
+        print(cls.principal)
+
+    @classmethod
+    def change_principal(cls,newp):
+        cls.principal=newp
+
+print(Teacher.__dict__)
+Teacher.change_principal("sybau")
+Teacher.display_deets()
+Teacher.change_principal("yo")
+Teacher.display_deets()
