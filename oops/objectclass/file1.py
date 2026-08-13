8980eb816af6d0c096a4ea43b33752802369fc74
Manasvi <manasviisonone@gmail.com>
2026-08-13 23:13:33 +0530

    Update file1.py

--- oops/objectclass/file1.py
+++ oops/objectclass/file1.py
@@ -1,5 +1,5 @@
 class pen:
-    pass
+    pass 
 
 p=pen.__new__(pen)  #step1 __new__ creates empty pen object and returns it
 pen.__new__(pen)      #step2 __init__  will initialise the empty pen instance
