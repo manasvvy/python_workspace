dff803a8d24402518e47a235c52a854bb80cdb94
Manasvi <manasviisonone@gmail.com>
2026-08-14 21:18:06 +0530

    Update file6.py

--- oops/objectclass/file6.py
+++ oops/objectclass/file6.py
@@ -7,7 +7,7 @@ class student:
         if isinstance(other,student):  #ensures only student type instance are compared
             return self.roll==other.roll
         return False
-
+ 
 s1=student(14,"m")        
 s2=student(14,"m")      
 print(s1==s2) #s1__eq__(s2)
