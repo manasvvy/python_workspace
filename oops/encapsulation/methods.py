831489c9e350d6c97ad5cab81266bf87215a57f8
manasvvy <manasviisonone@gmail.com>
2026-08-19 22:26:54 +0530

    modified methods

--- oops/encapsulation/methods.py
+++ oops/encapsulation/methods.py
@@ -1,32 +1,66 @@
 #setter and getter methods
 
 class student:
-   def __init__(self):
-      self.__marks=0 #pvt iv
-      self.__fees=0
+    def __init__(self):
+        self.__marks = 0
+        self.__fees = 0
 
-   def get_marks(self): 
-      return self.__marks
+    def get_marks(self):
+        return self.__marks
 
-   def set_marks(self,marks):
-    if marks>=0 and marks<=100: #validation logic
-       self.__marks=marks       #initialising logic
-    else:
-       print("invalid")   
+    def set_marks(self, marks):
+        if marks >= 0 and marks <= 100:   # validation logic
+            self.__marks = marks          # initializing logic
+        else:
+            print("invalid")
 
-   def get_fees(self):
-      return self.__fees
+    def get_fees(self):
+        return self.__fees
 
-   def set_fees(self):
-      if fees>=0 and fees<=50000000:
-         self.__fees=fees
-      else:
-         print("go to other school")   
-          
+    def set_fees(self, fees):
+        if fees >= 0 and fees <= 5000000:
+            self.__fees = fees
+        else:
+            print("go to other school")
 
-s1=student()
-s1.set_marks(88)           
+
+s1 = student()
+s1.set_marks(88)
 print(s1.get_marks())
-s1.set_fees(800000000000008)           
+s1.set_fees(200000)
 print(s1.get_fees())
- 
+
+print("__________________________________________________________________")
+
+class BankAccount:
+    def __init__(self):
+        self.__balance = 30000       #private iv
+
+    def deposit(self, amount):
+        if amount >= 0:
+            self.__balance += amount  #private iv
+        else:
+            print("Invalid deposit amount")
+
+    def withdraw(self, amount):
+        if amount >= 0 and amount <= self.__balance:
+            self.__balance -= amount
+        else:
+            print("Cannot withdraw")
+
+    def get_balance(self):
+        return self.__balance
+
+
+b1 = BankAccount()
+
+print(b1.get_balance())
+
+b1.deposit(20000)
+print(b1.get_balance())
+
+b1.withdraw(50000)
+print(b1.get_balance())
+
+b1.withdraw(5000)
+print(b1.get_balance())
