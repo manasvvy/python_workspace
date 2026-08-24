3f805605bf2ef792a5e8a601e71817c07daf10b2
Manasvi <manasviisonone@gmail.com>
2026-08-24 22:36:44 +0530

    Update file1.py

--- exception/file1.py
+++ exception/file1.py
@@ -4,6 +4,6 @@ print(3)
 try:
     print(23/0) #runtime error / exception
 except ZeroDivisionError:
-    print("handled")
+    print("handled") 
 print(4)  #below lines wont be executed bc its abnormal flow
 print(5)
