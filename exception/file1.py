f57048c20829539e2a08397a30a4e222a6c5efc3
manasvvy <manasviisonone@gmail.com>
2026-08-25 10:37:30 +0530

    add n modify

--- exception/file1.py
+++ exception/file1.py
@@ -1,9 +1,12 @@
 print(1)
 print(2)
 print(3)
+l=[10,20,30]
 try:
-    print(23/0) #runtime error / exception
-except ZeroDivisionError:
-    print("handled") 
+    print(l[4]) #runtime error / exception
+    print("line1")
+except IndexError as e:
+    print(8/0)
+    print("handled")
 print(4)  #below lines wont be executed bc its abnormal flow
 print(5)
---
+++ exception/file2.py
@@ -0,0 +1,10 @@
+print("start")
+s="muskan"
+try:
+    n1= int(input("enter no 1"))
+    n2= int(input("enter no 2"))
+    print(n1/n2)
+    print(s[n2])
+except (ZeroDivisionError IndexError ValueError) as e:
+    print(e)
+
