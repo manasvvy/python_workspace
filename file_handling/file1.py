ba9d3ae9b0af9481962aae9c3a7565157c4bd764
Manasvi <manasviisonone@gmail.com>
2026-08-27 10:50:31 +0530

    Update file1.py

--- file_handling/file1.py
+++ file_handling/file1.py
@@ -2,7 +2,7 @@ f=open("file_handling/demo1.txt","r") #file object is created
 print(f.name)
 print(f.closed)
 print(f.mode)
-print(f.readable())
+print(f.readable()) 
 print(f.writable())
 #print(f.read(5))
 print(f.readline(8))
